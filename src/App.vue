ebaada3277a80348b030d2c86b1a12c0f34968c3
Meshach <116318961+Meshach88@users.noreply.github.com>
2024-10-29 00:31:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+</script>
+
+<template>
+  <Navbar />
+  <Hero /> <!-- title="Test title"  subtitle="Test sub-title" -->
+  <HomeCards/>
+</template>

c92bd4a84153b6e38fe7ad140ec11e9b8fa52df1
Meshach <116318961+Meshach88@users.noreply.github.com>
2024-10-30 01:10:22 +0100

    added AddView file

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,8 +1,9 @@
 <script setup>
-import jobData from '@/jobs1.json'
-import { ref,defineProps } from 'vue';
+import { ref, reactive, defineProps, onMounted } from 'vue';
 import JobListing from '@/components/JobListing.vue'
 import { RouterLink } from 'vue-router';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+import axios from 'axios';
 
 defineProps({
     limit:Number,
@@ -11,9 +12,23 @@ defineProps({
         default:false
     }
 })
-const jobs = ref(jobData)
+// const jobs = ref([])
+const state = reactive({
+    jobs:[],
+    isLoading:true
+})
 
-
+onMounted(async () => {
+    try {
+        const response = await axios.get('/api/jobs');
+        state.jobs = response.data;
+    } catch (error) {
+        console.error('Error fetching jobs', error);
+    } finally{
+        state.isLoading = false;
+    }
+    
+})
 </script>
 
 <template>
@@ -22,8 +37,14 @@ const jobs = ref(jobData)
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
                 Browse Jobs
             </h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job"/>
+            <!-- Show loading spinner while loading is true -->
+             <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader/>
+             </div>
+
+             <!-- Show job listing when done loading -->
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job"/>
             </div>
         </div>
     </section>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "@/views/HomeView.vue";
 import JobsViews from "@/views/JobsViews.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
+import JobView from "@/views/JobView.vue";
+import AddJobView from "@/views/AddJobView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +18,21 @@ const router = createRouter({
       path: "/jobs",
       name: "jobs",
       component: JobsViews,
+    },
+    {
+      path: '/jobs/:id',
+      name: 'job',
+      component: JobView
+    },
+    {
+      path: '/jobs/add',
+      name: 'add-job',
+      component: AddJobView
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "notfound",
+      component: NotFoundView,
     },
   ],
 });
